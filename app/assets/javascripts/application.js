ba7b73c7e5fa1160900b2f1af4e259b6855756ad
katguz3485 <katarzyna.guzik01@gmail.com>
2018-06-16 17:06:47 +0200

    some stylesheets added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,11 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-// = require rails-ujs
-// = require jquery
-// = require jquery_ujs
-// = require activestorage
-// = require turbolinks
-// = require bootstrap-sprockets
-// = require_tree .
+//=
+//= require rails-ujs
+//= require activestorage
+//= require jquery3
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require turbolinks
+//= require_tree .
