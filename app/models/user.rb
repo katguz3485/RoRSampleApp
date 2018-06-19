9cb343ed9e46dcb883dfd19c3e4091707fdd55e6
katguz3485 <katarzyna.guzik01@gmail.com>
2018-06-19 23:06:20 +0200

    validations added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  validates :username, presence: true,
+            uniquness: {case_sensitive: false},
+            length: {minimum: 3, maximum: 30}
+
+  VALID_EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
+  validates :email, presence: true, length: {maximum: 105},
+            uniqueness: {case_sensitive: false}, format: { with: VALID_EMAIL_REGEX }
+
+
 end
