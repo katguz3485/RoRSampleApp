0e677b1b4e7181837777e7da623e26000e161aa0
katguz3485 <katarzyna.guzik01@gmail.com>
2018-06-22 16:10:38 +0200

    added edit and update user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,74 +1,56 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
-  def edit
+  def index
+    @users = User.all
   end
 
-  # POST /users
-  # POST /users.json
+
   def create
+    debugger
+
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if user.save
+      flash[:success] = "Welcome in our blog #{@user.username}"
+      redirect_to articles_path
+    else
+      render 'new'
     end
+
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
+  def show
+  end
+
+  def edit
+
+  end
+
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      flash[:success] = "Your account was succcesfully updated"
+      redirect_to articles_path
+    else
+      flash[:danger] = "Error, please try again"
+      render 'edit'
+
     end
+
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:username, :email)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
 
-  resources :users
+  resources :users, except: [:new]
   root 'articles#index'
   get 'pages/index'
+  get 'signup', to: 'users#new'
   get 'about', to: 'pages#about'
   resources :articles do
     resources :comments
+
+
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
