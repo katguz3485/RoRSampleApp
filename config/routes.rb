99e99cd5548a7097284b18a4ce7603e785415cef
katguz3485 <katarzyna.guzik01@gmail.com>
2018-07-02 00:16:19 +0200

    empty sad commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 
-  root 'pages#home'
+
   resources :categories, except: [:destroy]
   resources :users, except: [:new]
   resources :pages
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles do
     resources :comments
-  end
+    end
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
