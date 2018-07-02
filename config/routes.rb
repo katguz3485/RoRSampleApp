3931468e3ff6c4c69088c2690520293e7219d7d7
katguz3485 <katarzyna.guzik01@gmail.com>
2018-07-02 16:00:07 +0200

    bootstrap added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-
+  root 'pages#home'
 
   resources :categories, except: [:destroy]
   resources :users, except: [:new]
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
     resources :comments
   end
 
-  root 'pages#home'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
